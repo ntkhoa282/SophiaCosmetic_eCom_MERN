6df3a0b2f6e520c747769c7c1297c40edd104108
ntkhoa282 <nguyenthienkhoa282@gmail.com>
2022-12-10 00:05:28 +0700

    update income chart

--- sophia_fe/src/pages-admin/Stats-admin/IncomeStats/IncomeStats.js
+++ sophia_fe/src/pages-admin/Stats-admin/IncomeStats/IncomeStats.js
@@ -2,6 +2,7 @@ import classNames from 'classnames/bind';
 import { useEffect, useState } from 'react';
 import httpRequest from '~/ultis/httpRequest';
 import styles from './IncomeStats.module.scss';
+import { BarChart, Bar, XAxis, YAxis, Legend, Tooltip } from 'recharts';
 
 const cx = classNames.bind(styles);
 
@@ -16,13 +17,34 @@ function IncomeStats() {
         getStats();
     }, []);
 
+    const chartData = incomeStats.map((stat) => ({ month: stat._id, income: stat.total }));
+
     return (
         <div className={cx('container')}>
             <div className={cx('title')}>
                 <h3>Thống kê doanh thu</h3>
             </div>
             <div className={cx('mt-4', 'stats-table')}>
-                <table>
+                <div className={cx('chart')}>
+                    <BarChart
+                        data={chartData}
+                        width={1000}
+                        height={300}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <XAxis dataKey="month" />
+                        <YAxis width={200} />
+                        <Legend />
+                        <Tooltip />
+                        <Bar dataKey="income" fill="#37ad37" />
+                    </BarChart>
+                </div>
+                <table className={cx('mt-4')}>
                     <thead>
                         <tr>
                             <td>STT</td>
